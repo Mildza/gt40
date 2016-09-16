4b021b7d19319aad33e2efeb8de09b9dca25a558
Mildza <mildzan@gmail.com>
2016-09-16 14:39:04 +0200

    start

---
+++ config.rb
@@ -0,0 +1,11 @@
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "stylesheets"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
