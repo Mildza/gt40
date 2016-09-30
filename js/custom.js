60b8a2d56def20243fb7e44b9a3da20e3a6aaa2c
Mildza <mildzan@gmail.com>
2016-09-30 14:54:35 +0200

    close button

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready( function($){
+    $('.carousel').carousel();
+    $('.collapsed-image .fa').on('click', function(){
+        $(this).parent().parent().parent().removeClass("in");
+    });
+});
